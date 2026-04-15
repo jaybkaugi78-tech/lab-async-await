e0a19a4e4b6050b17017cd3e2e20dae96a9834ef
Jeremy Gitonga <jaybkaugi78@gmail.com>
2026-04-15 23:45:03 +0300

    lab submitted

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your code here!
+function displayPosts(posts) {
+  const ul = document.getElementById('post-list');
+
+  posts.forEach(post => {
+    const li = document.createElement('li');
+
+    const h1 = document.createElement('h1');
+    h1.textContent = post.title;
+
+    const p = document.createElement('p');
+    p.textContent = post.body;
+
+    li.appendChild(h1);
+    li.appendChild(p);
+    ul.appendChild(li);
+  });
+}
+
+async function getPosts() {
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const posts = await response.json();
+  displayPosts(posts);
+}
+
+getPosts();
+
+module.exports = { getPosts, displayPosts };
